3cb4cd6105e57d2f874a0b22df2cb013dd7159cc
JM2308 <wlals2987@naver.com>
2023-02-09 15:41:18 +0900

    Feat: SignupFinishActivity

--- app/src/main/java/cmc/sole/android/Signup/SignupFinishActivity.kt
+++ app/src/main/java/cmc/sole/android/Signup/SignupFinishActivity.kt
@@ -1,12 +1,22 @@
 package cmc.sole.android.Signup
 
+import cmc.sole.android.MainActivity
 import cmc.sole.android.Utils.BaseActivity
 import cmc.sole.android.databinding.ActivitySignupAgreeBinding
 import cmc.sole.android.databinding.ActivitySignupFinishBinding
 import cmc.sole.android.databinding.ActivitySignupNicknameBinding
 
 class SignupFinishActivity: BaseActivity<ActivitySignupFinishBinding>(ActivitySignupFinishBinding::inflate) {
-    override fun initAfterBinding() {
 
+    // UPDATE: 회원가입 API 연동 필요!
+    override fun initAfterBinding() {
+        initClickListener()
+    }
+
+    private fun initClickListener() {
+        binding.signupFinishNextBtn.setOnClickListener {
+            changeActivity(MainActivity::class.java)
+            finishAffinity()
+        }
     }
 }
