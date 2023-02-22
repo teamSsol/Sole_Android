9902cc1e2a4da94c4c403d98714e94e7b8dcc1ef
JM2308 <wlals2987@naver.com>
2023-02-23 00:39:19 +0900

    Feat: Connect Naver Map API

--- app/src/main/java/cmc/sole/android/MapFragment.kt
+++ app/src/main/java/cmc/sole/android/MapFragment.kt
@@ -1,9 +1,23 @@
 package cmc.sole.android
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import cmc.sole.android.Utils.BaseFragment
 import cmc.sole.android.databinding.FragmentMapBinding
+import com.naver.maps.map.NaverMapSdk
 
 class MapFragment: BaseFragment<FragmentMapBinding>(FragmentMapBinding::inflate) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        NaverMapSdk.getInstance(requireActivity()).client = NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NAVER_CLIENT_ID)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
     override fun initAfterBinding() {
         
     }
