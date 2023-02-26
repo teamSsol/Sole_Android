1f79ea776828d35b9cd382a4577b37a289310c49
JM2308 <wlals2987@naver.com>
2023-02-27 00:45:10 +0900

    Fix: NaverMapAPI ClientId Error

--- app/src/main/java/cmc/sole/android/CourseDetailActivity.kt
+++ app/src/main/java/cmc/sole/android/CourseDetailActivity.kt
@@ -1,36 +1,28 @@
 package cmc.sole.android
 
 import android.os.Bundle
-import cmc.sole.android.MyCourse.MyCourseFragment
+import androidx.appcompat.app.AppCompatActivity
 import cmc.sole.android.Utils.BaseActivity
 import cmc.sole.android.databinding.ActivityCourseDetailBinding
 import com.naver.maps.map.NaverMap
-import com.naver.maps.map.NaverMapSdk
 import com.naver.maps.map.OnMapReadyCallback
 
 
-class CourseDetailActivity: BaseActivity<ActivityCourseDetailBinding>(ActivityCourseDetailBinding::inflate),
+// class CourseDetailActivity: BaseActivity<ActivityCourseDetailBinding>(ActivityCourseDetailBinding::inflate),
+class CourseDetailActivity: AppCompatActivity(),
     OnMapReadyCallback {
 
-    override fun initAfterBinding() {
-
-    }
+    lateinit var binding: ActivityCourseDetailBinding
+    lateinit var naverMap: NaverMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityCourseDetailBinding.inflate(layoutInflater)
+
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync(this)
+
         setContentView(R.layout.activity_course_detail)
-
-        NaverMapSdk.getInstance(this).client = NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NAVER_CLIENT_ID)
-
-        val fm = supportFragmentManager
-//        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
-//            ?: MapFragment().also {
-//                fm.beginTransaction().add(R.id.map, it).commit()
-//            }
-
-        val mapFragment = fragmentManager.findFragmentById(R.id.map) as MyCourseFragment
-
-        // mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(p0: NaverMap) {
