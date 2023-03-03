bbbf402eddd47b9b5d2429f751e87161861aae30
JM2308 <wlals2987@naver.com>
2023-03-03 19:59:14 +0900

    Feat: MyPageInfoSettingActivity View

---
+++ app/src/main/java/cmc/sole/android/Home/MyPage/MyPageInfoSettingActivity.kt
@@ -0,0 +1,10 @@
+package cmc.sole.android.Home.MyPage
+
+import cmc.sole.android.Utils.BaseActivity
+import cmc.sole.android.databinding.ActivityMyPageInfoSettingBinding
+
+class MyPageInfoSettingActivity: BaseActivity<ActivityMyPageInfoSettingBinding>(ActivityMyPageInfoSettingBinding::inflate) {
+    override fun initAfterBinding() {
+
+    }
+}
