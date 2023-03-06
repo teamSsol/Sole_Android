f98cb1467bc4f36b498db3aaf8f89d50d5ed1510
JM2308 <wlals2987@naver.com>
2023-03-06 18:39:08 +0900

    Fix: MyPage Logout and Withdrawal Dialog Width/Height

--- app/src/main/java/cmc/sole/android/Home/MyPage/DialogMyPageWithdrawal.kt
+++ app/src/main/java/cmc/sole/android/Home/MyPage/DialogMyPageWithdrawal.kt
@@ -1,9 +1,13 @@
 package cmc.sole.android.Home.MyPage
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import cmc.sole.android.databinding.DialogMyPageWithdrawalBinding
@@ -18,10 +22,18 @@ class DialogMyPageWithdrawal: DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DialogMyPageWithdrawalBinding.inflate(inflater, container, false)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.setGravity(Gravity.CENTER)
 
         initClickListener()
 
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        dialog?.window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        dialog?.window?.setGravity(Gravity.CENTER_HORIZONTAL or Gravity.CENTER_VERTICAL)
     }
 
     private fun initClickListener() {
