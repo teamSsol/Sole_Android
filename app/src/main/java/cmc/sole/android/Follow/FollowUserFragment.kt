e44296aacb0bb5006e8279cb8c327eb7cad06d1d
JM2308 <wlals2987@naver.com>
2023-03-09 01:16:53 +0900

    Feat: FollowUserFragment View

---
+++ app/src/main/java/cmc/sole/android/Follow/FollowUserFragment.kt
@@ -0,0 +1,10 @@
+package cmc.sole.android.Follow
+
+import cmc.sole.android.Utils.BaseFragment
+import cmc.sole.android.databinding.FragmentFollowUserBinding
+
+class FollowUserFragment: BaseFragment<FragmentFollowUserBinding>(FragmentFollowUserBinding::inflate) {
+    override fun initAfterBinding() {
+        
+    }
+}
